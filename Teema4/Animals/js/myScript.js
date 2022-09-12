148cdf296ec76a28b8f7f037e1faddce86d25851
Maksim Dzjubenko <90320393+SvinDzjuba@users.noreply.github.com>
2022-09-12 11:22:53 +0300

    idi naher

---
+++ Teema4/Animals/js/myScript.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    console.log(category)
+    console.log(animals)
+    for (let i = 0; i < category.length; i++) {
+        var li = document.createElement('li');
+        li.innerHTML = ('<a href="#" onClick="myContent(´' + category[i] + '´)" >' + category[i] + '</a>');
+        document.getElementById('menu').appendChild(li);
+    }
+});
+function myContent(val) {
+    let heading = '<h2>' + val + '</h2>';
+    let text = '';
+
+    let newAnimals = animals.filter(animal => animal.classname === val);
+    console.log(newAnimals.length);
+}
