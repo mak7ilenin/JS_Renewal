286afbce1b62f9d0d7314a9ae7946a3b33ec4ae2
Maksim Dzjubenko <90320393+SvinDzjuba@users.noreply.github.com>
2022-09-14 22:20:07 +0300

    Made a filter on categories and agents load when document is ready

--- Teema4/HomePage/js/main.js
+++ Teema4/HomePage/js/main.js
@@ -1,37 +1,48 @@
+$(document).ready(function() {
+    showAgents(agents)
+});
+
 // Categories output
 let list = document.createElement('ul');
-let category = ''
 
 for (let i = 0; i < categories.length; i++) {
-    category = document.createElement('li');
-    category.innerHTML = categories[i];
+    let category = document.createElement('li');
+    category.innerHTML = '<a href="#" onClick="chooseCategory(`' + categories[i] + '`)" >' + categories[i] + '</a>';
     list.appendChild(category);
 }
-document.getElementById('content').appendChild(list);
+$('header').append(list);
 
-// Agents information output
-let agent = '';
-let abilities = agents.map(agent => agent.abilities);
+function chooseCategory(category) {
+    // Show agents by this role
+    let filteredAgents = agents.filter(agent => agent.role === category);
+    showAgents(filteredAgents)
+}
 
-for (let i = 0; i < agents.length; i++) {
-    agent += '<div class="agent">'
-        agent += '<div class="img_container">'
-            agent += '<img src="images/' + agents[i].picture + '" alt="' + agents[i].name + '" />'
-        agent += '</div>';
-        agent += '<div class="info_container">'
-            agent += '<h3>' + agents[i].name + '</h3>';
-            agent += '<p>' + agents[i].biography + '</p>';
-            agent += '<h4>Abilitites</h4>';
-            agent += '<div class="ability">';
-                let ability = abilities[i].map(ability => ability);
-                let abilityInfo = ability.map(ability => ability);
-                for (let j = 0; j < ability.length; j++) {
-                    agent += '<p>Key: ' + abilityInfo[j].key + '</p>';
-                    agent += '<p>Name: ' + abilityInfo[j].name + '</p>';
-                    agent += '<p>Description: ' + abilityInfo[j].description + '</p><br>';
-                }
+function showAgents(agents) {
+    // Agents information output
+    let agent = '';
+    let abilities = agents.map(agent => agent.abilities);
+    for (let i = 0; i < agents.length; i++) {
+        agent += '<div class="agent">'
+            agent += '<div class="decoration_line1"></div>'
+            agent += '<div class="decoration_line2"></div>'
+            agent += '<div class="img_container">'
+                agent += '<img src="images/' + agents[i].picture + '" alt="' + agents[i].name + '" />'
+            agent += '</div>';
+            agent += '<div class="info_container">'
+                agent += '<h3>' + agents[i].name + '</h3>';
+                agent += '<p>' + agents[i].biography + '</p>';
+                agent += '<h4>Abilitites</h4>';
+                agent += '<div class="ability">';
+                    let ability = abilities[i].map(ability => ability);
+                    let abilityInfo = ability.map(ability => ability);
+                    for (let j = 0; j < ability.length; j++) {
+                        agent += '<p>' + abilityInfo[j].key + ' - ' + abilityInfo[j].name + '</p>';
+                        agent += '<p>Description: ' + abilityInfo[j].description + '</p><br>';
+                    }
+                agent += '</div>';
             agent += '</div>';
         agent += '</div>';
-    agent += '</div>';
+    }
+    $('#content').html(agent);
 }
-document.getElementById('content').innerHTML += agent;
