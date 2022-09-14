52eb9f9ada4efc0044208ff9ae5b181f14e13dc1
Maksim Dzjubenko <90320393+SvinDzjuba@users.noreply.github.com>
2022-09-14 19:27:48 +0300

    Parsed the js objects into html

--- Teema4/HomePage/js/main.js
+++ Teema4/HomePage/js/main.js
@@ -10,24 +10,28 @@ for (let i = 0; i < categories.length; i++) {
 document.getElementById('content').appendChild(list);
 
 // Agents information output
-let agentContainer = document.createElement('div');
 let agent = '';
+let abilities = agents.map(agent => agent.abilities);
 
 for (let i = 0; i < agents.length; i++) {
-    agent += '<div class="img_container">'
-        agent += '<img src="images/' + agents[i].picture + '" alt="' + agents[i].name + '" />'
-    agent += '</div>';
-    agent += '<div class="info_container">'
-        agent += '<h3>' + agents[i].name + '</h3>';
-        agent += '<p>' + agents[i].biography + '</p>';
-        agent += '<h4>Abilitites</h4>';
-        agent += '<div class="ability">';
-        let ability = agents.find(agent => agent.abilities);
-        console.log(ability);
-            agent += '<p>Key: ' + ability[i].key + '</p>';
-            agent += '<p>Name: ' + ability[i].name + '</p>';
-            agent += '<p>Key: ' + ability[i].description + '</p>';
+    agent += '<div class="agent">'
+        agent += '<div class="img_container">'
+            agent += '<img src="images/' + agents[i].picture + '" alt="' + agents[i].name + '" />'
         agent += '</div>';
+        agent += '<div class="info_container">'
+            agent += '<h3>' + agents[i].name + '</h3>';
+            agent += '<p>' + agents[i].biography + '</p>';
+            agent += '<h4>Abilitites</h4>';
+            agent += '<div class="ability">';
+                let ability = abilities[i].map(ability => ability);
+                let abilityInfo = ability.map(ability => ability);
+                for (let j = 0; j < ability.length; j++) {
+                    agent += '<p>Key: ' + abilityInfo[j].key + '</p>';
+                    agent += '<p>Name: ' + abilityInfo[j].name + '</p>';
+                    agent += '<p>Description: ' + abilityInfo[j].description + '</p><br>';
+                }
+            agent += '</div>';
+        agent += '</div>';
+    agent += '</div>';
 }
-agentContainer.innerHTML = agent;
-document.getElementById('content').appendChild(agentContainer);
+document.getElementById('content').innerHTML += agent;
