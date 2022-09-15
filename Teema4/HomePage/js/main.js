3e9c06d251ff5c366aa27b3116e7146a6c82bf04
Maksim Dzjubenko <90320393+SvinDzjuba@users.noreply.github.com>
2022-09-15 11:07:49 +0300

    Made a dropdown and added some stuff as well.

--- Teema4/HomePage/js/main.js
+++ Teema4/HomePage/js/main.js
@@ -1,57 +1,22 @@
-$(document).ready(function() {
-    // Load agents after the page load
-    showAgents(agents);
-    function checkPosition() {
-        let elements = $('.agent');
-        let windowHeight = window.innerHeight;
-        for (var i = 0; i < elements.length; i++) {
-            let element = elements[i];
-            let positionFromTop = elements[i].getBoundingClientRect().top;
-        
-            if(positionFromTop - windowHeight <= 0) {
-                element.classList.remove('hidden');
-                element.classList.add('fade-in-element');
-                // setTimeout(() => {
-                //     element.classList.remove('fade-in-element');
-                // }, 1300);
-            }
-        }
-    }
-    window.addEventListener('scroll', checkPosition);
-    checkPosition();
-});
-
-// Categories output
-let list = document.createElement('ul');
-
-for (let i = 0; i < categories.length; i++) {
-    let category = document.createElement('li');
-    category.innerHTML = '<a href="#" onClick="chooseCategory(`' + categories[i] + '`)" >' + categories[i] + '</a>';
-    list.appendChild(category);
-}
-$('header').append(list);
-
-function chooseCategory(category) {
-    // Show agents by this role
-    let filteredAgents = agents.filter(agent => agent.role === category);
-    showAgents(filteredAgents)
-}
-
+// Agents information output
 function showAgents(agents) {
-    // Agents information output
-    let agent = '';
+    let agent = '<h2>Valorant Agents</h2>';
     let abilities = agents.map(agent => agent.abilities);
     for (let i = 0; i < agents.length; i++) {
         agent += '<div class="agent">'
             agent += '<div class="decoration_line1"></div>'
             agent += '<div class="img_container">'
-                agent += '<img src="images/' + agents[i].picture + '" alt="' + agents[i].name + '" />'
+                agent += '<a data-title="Agent" data-lightbox="image" href="images/' + agents[i].picture + '"><img src="images/' + agents[i].picture + '" alt="' + agents[i].name + '" /></a>'
             agent += '</div>';
             agent += '<div class="info_container">'
                 agent += '<h3>' + agents[i].name + '</h3>';
+                agent += '<p class="agent_role">' + agents[i].role + '</p>'
                 agent += '<p>' + agents[i].biography + '</p>';
-                agent += '<h4>Abilitites</h4>';
-                agent += '<div class="ability">';
+                agent += '<div class="ability_container">';
+                    agent += '<h4>Abilitites</h4>';
+                    agent += '<div class="ability_arrow"><img src="images/components/pngwing.com.png" class="arrow"></div>';
+                agent += '</div>';
+                agent += '<div class="abilities_container">';
                     let ability = abilities[i].map(ability => ability);
                     let abilityInfo = ability.map(ability => ability);
                     for (let j = 0; j < ability.length; j++) {
@@ -64,3 +29,50 @@ function showAgents(agents) {
     }
     $('#content').html(agent);
 }
+
+// Show agents by this role
+function chooseCategory(category) {
+    let filteredAgents = agents.filter(agent => agent.role === category);
+    showAgents(filteredAgents)
+}
+
+$(document).ready(function() {
+    // Load agents after the page load
+    showAgents(agents);
+    
+    // Check every agent container position to animate
+    function checkPosition() {
+        let elements = $('.agent');
+        let windowHeight = window.innerHeight;
+        for (var i = 0; i < elements.length; i++) {
+            let element = elements[i];
+            let positionFromTop = elements[i].getBoundingClientRect().top;
+            
+            if(positionFromTop - windowHeight <= 0) {
+                element.classList.remove('hidden');
+                element.classList.add('fade-in-element');
+            }
+        }
+    }
+    window.addEventListener('scroll', checkPosition);
+    checkPosition();
+
+    // Abilities dropdown
+    $('.abilities_container').slideUp(10);
+    $('.ability_container').click(function() {
+        $(this).next().slideToggle(400, 'swing');
+        const prev = $(this).prev();
+        const arrow = prev.prev().next().next().children().children();
+        arrow.toggleClass('arrow-up');
+    });
+
+    // Categories output
+    let list = document.createElement('ul');
+    for (let i = 0; i < categories.length; i++) {
+        let category = document.createElement('li');
+        category.innerHTML = '<a href="#" onClick="chooseCategory(`' + categories[i] + '`)" >' + categories[i] + '</a>';
+        list.appendChild(category);
+    }
+    $('header').append(list);
+
+});
